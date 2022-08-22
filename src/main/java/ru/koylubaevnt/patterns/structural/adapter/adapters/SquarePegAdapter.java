442b187af13efed34babc84df32bb1f86c77c592
Koylubaev Nikita <koilubaev@dev.vtb.ru>
2022-08-22 22:22:21 +0500

    Добавил "Структурный паттерн": "Адаптер".

---
+++ src/main/java/ru/koylubaevnt/patterns/structural/adapter/adapters/SquarePegAdapter.java
@@ -0,0 +1,24 @@
+package ru.koylubaevnt.patterns.structural.adapter.adapters;
+
+import ru.koylubaevnt.patterns.structural.adapter.round.RoundPeg;
+import ru.koylubaevnt.patterns.structural.adapter.square.SquarePeg;
+
+/**
+ * Адаптер позволяет использовать КвадратныеКолышки и КруглыеОтверстия вместе.
+ */
+public class SquarePegAdapter extends RoundPeg {
+
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Рассчитываем минимальный радиус, в который пролезет этот колышек.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
